72dfdd066a207e9997aeb04cd1b6dd8ae586d051
DHL <dhl1402@gmail.com>
2018-05-17 10:35:17 +0700

    Initial Commit

---
+++ Objective-C Playground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C Playground
+//
+//  Created by DHL on 5/17/18.
+//  Copyright © 2018 DHL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
