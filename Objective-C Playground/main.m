606338d0136171c8f3a2acb0953779f442b5a525
Đặng Hoàng Long <longdh@appota.com>
2018-06-22 11:53:59 +0700

    Declare/call method

--- Objective-C Playground/main.m
+++ Objective-C Playground/main.m
@@ -1,6 +1,17 @@
 #import <Foundation/Foundation.h>
 
+@interface NumberUtil:NSObject
+- (int)sum:(int)num1 withNum:(int)num2;
+@end
+
+@implementation NumberUtil
+- (int)sum:(int)num1 withNum:(int)num2 {
+  return num1 + num2;
+}
+@end
+
 int main() {
-  NSLog(@"Hello word");
+  NumberUtil *nu = [[NumberUtil alloc] init];
+  NSLog(@"%d", [nu sum:1 withNum:2]);
   return 0;
 }
