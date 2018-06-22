03ba838d46d8725df4e4b4ddc6fe203d5dd8977e
Đặng Hoàng Long <longdh@appota.com>
2018-06-22 11:44:38 +0700

    Init

--- Objective-C Playground/main.m
+++ Objective-C Playground/main.m
@@ -1,17 +1,6 @@
-//
-//  main.m
-//  Objective-C Playground
-//
-//  Created by DHL on 5/17/18.
-//  Copyright © 2018 DHL. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+int main() {
+  NSLog(@"Hello word");
+  return 0;
 }
