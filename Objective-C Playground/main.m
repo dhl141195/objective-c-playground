b951f65a7775d08ff9e825da4f403d174a9d6c56
Đặng Hoàng Long <longdh@appota.com>
2018-06-27 09:47:32 +0700

    no param, omit return type

--- Objective-C Playground/main.m
+++ Objective-C Playground/main.m
@@ -1,16 +1,13 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char *argv[]) {
-  // Declare the block variable
-  double (^distanceFromRateAndTime)(double rate, double time);
+  NSString* (^saySomething)(void);
   
-  // Create and assign the block
-  distanceFromRateAndTime = ^double(double rate, double time) {
-    return rate * time;
+  saySomething = ^{
+    return @"Im a block";
   };
-  // Call the block
-  double dx = distanceFromRateAndTime(35, 1.5);
-  
-  NSLog(@"A car driving 35 mph will travel %.2f miles in 1.5 hours.",dx);
+
+  NSLog(@"%@", saySomething());
+
   return 0;
 }
