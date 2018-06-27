3304d32b614d247163236b12283dbb93f6476b26
Đặng Hoàng Long <longdh@appota.com>
2018-06-27 10:50:52 +0700

    link outer-scope variable using __block modifier

--- Objective-C Playground/main.m
+++ Objective-C Playground/main.m
@@ -1,14 +1,16 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char *argv[]) {
-  int i = 0;
+  __block int i = 0;
 
   void (^testClosure)(void) = ^{
     NSLog(@"%d", i);
+    i = 2;
   };
 
   i = 1;
-  testClosure(); // print 0
+  testClosure(); // print 1
 
+  NSLog(@"%d", i); // print 2
   return 0;
 }
